75b9c80ee7d31ed50e20894c644217a596bb57be
Shubham <guptashubham95a@gmail.com>
2023-03-06 14:29:20 +0530

    added UI

---
+++ userfrontend/src/App.js
@@ -0,0 +1,21 @@
+
+import TableDemo from "./TableDemo";
+
+
+
+function App() {
+  return (
+    <div>
+     <h1
+              style={{
+                 display: 'flex', justifyContent: 'center', padding: '15px',
+                 border: '13px solid #b4f0b4', color: 'rgb(11, 167, 11)'
+                }}>
+            Users Table
+        </h1>
+            <TableDemo />
+    </div>
+  );
+}
+
+export default App;
